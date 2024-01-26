1c4437110e24f80f2bcea81530d9b2bb532784d7
Vengleab SO <sovengleab@gmail.com>
2024-01-26 18:28:16 +0700

    [demo] basic api and webapp with template engine

---
+++ backend-demo/README.md
@@ -0,0 +1,8 @@
+- Create nodejs as backend application
+- Basic route with Express
+- Create api for CRUD route for users with dummy data
+- Encrypt user's password with harse algorithm (md5, bcrypt)
+- Convert from dummy data to database's data
+- authentication (login)
+- generate token with jwt
+- create protected routes
---
+++ backend-demo/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) => {
+    // res.send({ "message" : "Hello World!" });
+    res.render("template", { payload : "data from server"});
+})
+app.get("/about-us", (req, res) => {
+    // res.send({ "message" : "Hello World!" });
+    res.render("template", { payload : "about data"});
+})
+
+
+app.listen(3000, () => {
+    console.log("API up and running at http://localhost:3000");
+});
